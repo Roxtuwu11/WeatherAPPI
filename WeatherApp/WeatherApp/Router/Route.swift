36294c890bb6c1ba963904df74edd9e27a013d48
Ximena Rotceh Mendoza Gamino <xmendoza@intercam.com.mx>
2025-05-15 16:29:52 -0600

    add functions to navigate

--- WeatherApp/WeatherApp/Router/Route.swift
+++ WeatherApp/WeatherApp/Router/Route.swift
@@ -11,6 +11,7 @@ weak var viewController: SearchViewController?
 
 @Observable
 class Router{
+    var routes: [Route] = []
     func navigateToResult()
     {
         let uiHost = UIHostingController(rootView: WeatherCurrentView())
@@ -18,5 +19,24 @@ class Router{
          viewController?.navigationController?.pushViewController(uiHost,
                                                                        animated: true)
     }
+    func navigateTo(route: Route) {
+        routes.append(route)
+    }
+    func unwind(_ route: Route)
+    {
+        guard let index = routes.firstIndex(where: { $0 == route }) else { return  }
+        routes = Array(routes.prefix(upTo: index + 1))
+    }
+    func popToRoot()
+    {
+        routes = []
+    }
+    
+}
+
+enum Route: Hashable {
+    case result
+    case detail
+    
 }
 
